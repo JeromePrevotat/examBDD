6d2678d3a4e330a78fd9c09660c9f3bc8b4cb1e6
unknown <jer.prevotat@gmail.com>
2025-03-20 16:05:51 +0100

    done - minor fixes inc

--- exam-3.sql
+++ exam-3.sql
@@ -22,27 +22,44 @@ ON chefs.restaurant_id = restaurants.id
 GROUP BY chefs.id;
 
 -- 5. Lister les chefs et le nombre de plats qu'ils ont créés.
-SELECT chefs.name, COUNT(dishes.chef_id) FROM chefs
+SELECT chefs.name, COUNT(dishes.chef_id) AS nb_creation FROM chefs
 JOIN dishes
 ON chefs.id = dishes.chef_id
 GROUP BY chef_id;
 
 -- 6. Lister les chefs qui ont créé plus d'un plat.
-SELECT chefs.name, COUNT(dishes.chef_id) FROM chefs
+SELECT chefs.name, COUNT(dishes.chef_id) AS nb_creation FROM chefs
 JOIN dishes
 ON chefs.id = dishes.chef_id
 GROUP BY dishes.chef_id
 HAVING (COUNT(dishes.chef_id) >= 2);
 
 -- 7. Calculez le nombre de chefs ayant créé un seul plat.
-SELECT chefs.name, COUNT(dishes.chef_id) FROM chefs
+SELECT chefs.name, COUNT(dishes.chef_id) AS nb_dish FROM chefs
 JOIN dishes
 ON chefs.id = dishes.chef_id
 GROUP BY dishes.chef_id
 HAVING (COUNT(dishes.chef_id) = 1);
 
 -- 8. Calculez le nombre de plats pour chaque type de cuisine.
---SELECT COUNT()
+SELECT restaurants.cuisine_type, COUNT(dishes.chef_id) AS nb_dish FROM restaurants
+JOIN chefs
+ON chefs.restaurant_id = restaurants.id
+JOIN dishes
+ON dishes.chef_id = chefs.id
+GROUP BY restaurants.cuisine_type;
 
 -- 9. Calculez le prix moyen des plats par type de cuisine.
+SELECT restaurants.cuisine_type, AVG(dishes.price) AS avg_price FROM restaurants
+JOIN chefs
+ON chefs.restaurant_id = restaurants.id
+JOIN dishes
+ON dishes.chef_id = chefs.id
+GROUP BY restaurants.cuisine_type;
+
 -- 10. Trouver le prix moyen des plats créés par chaque chef, en incluant seulement les chefs ayant créé plus de 2 plats
+SELECT chefs.name, COUNT(dishes.chef_id) AS nb_creation, AVG(dishes.price) AS avg_price  FROM chefs
+JOIN dishes
+ON chefs.id = dishes.chef_id
+GROUP BY dishes.chef_id
+HAVING (COUNT(dishes.chef_id) >= 2);
